74fc1d06e069436e5c9235bbeae792e1c0531967
Björn Oettinghaus <erblast2001@gmx.de>
2017-10-18 22:14:57 +0200

    create ordered factors when converting numericals to factors

--- tests/testthat/test_clean.R
+++ tests/testthat/test_clean.R
@@ -10,8 +10,10 @@ test_that('data_ls output format'
   expect_true( 'data'         %in% names(data_ls) )
   expect_true( 'numericals'   %in% names(data_ls) )
   expect_true( 'categoricals' %in% names(data_ls) )
+  expect_true( 'categoricals_ordered' %in% names(data_ls) )
   expect_true( 'ids'          %in% names(data_ls) )
   expect_true( 'boxcox_data'  %in% names(data_ls) )
   expect_true( 'boxcox_names' %in% names(data_ls) )
 
+  expect_true( is.ordered(data_ls$data$cyl) )
 })
