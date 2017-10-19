4dfc87325265d5fc5db0171395fbaeef27d562bd
Oettinghaus <Oettinghaus_B@holding.local>
2017-10-19 15:54:43 +0200

    model feats more tests added

--- R/f_model_feats.R
+++ R/f_model_feats.R
@@ -17,7 +17,7 @@
 #'
 #' @rdname f_model_importance_randomForest
 #' @export
-f_model_importance_randomForest = function(m){
+f_model_importance_randomForest = function(m, ...){
 
   if( ! inherits(m, 'randomForest') ){
     warning('model is not of class randomForest returning NULL')
@@ -44,24 +44,27 @@ f_model_importance_randomForest = function(m){
 
 #' @title extract variable importance for svm
 #' @param m model of class svm
+#' @param data original training dataframe
 #' @return dataframe
 #' @examples
 #'
 #'#regression
 #' m = twidlr::svm(mtcars, disp~.)
-#' f_model_importance_svm(m)
+#' f_model_importance_svm(m, mtcars)
 #'
 #' #classification
-#' data_ls = f_clean_data(mtcars)
-#' m = twidlr::svm(data_ls$data, cyl~.)
-#' f_model_importance_svm(m)
+#' data = mtcars
+#' data$cyl = factor(data$cyl, ordered = T)
+#' m = twidlr::svm(data, cyl~.)
+#' f_model_importance_svm(m, data)
+#'
 #'
 #' @rdname f_model_importance_svm
 #' @details uses 1D-SA 1 dimensional sensitivity analysis using
 #'   rminer::Importance()
 #' @export
 #' @importFrom rminer Importance
-f_model_importance_svm = function(m){
+f_model_importance_svm = function(m, data){
 
   if( ! inherits(m, 'svm') ){
     warning('model is not of class svm returning NULL')
@@ -72,7 +75,33 @@ f_model_importance_svm = function(m){
     as.character() %>%
     .[2]
 
-  imp = rminer::Importance(m, data, PRED = twidlr::predict.svm )
+  response_var_sym = as.name(response_var)
+
+  formula = m$terms %>%
+    as.character()
+
+  formula = paste0( formula[2], formula[1], formula[3] ) %>%
+    as.formula()
+
+  if( is.factor(data[[response_var]]) ){
+
+    # rminer requires only numericals and does not like tibbles
+    data = data %>%
+      mutate_if( is.character, factor, ordered = T ) %>%
+      mutate_all( as.numeric ) %>%
+      mutate( !!response_var_sym := factor( !!response_var_sym, ordered = T ) ) %>%
+      as.data.frame()
+
+
+    #classification
+    m = rminer::fit( formula, data, model = 'svm' )
+
+    imp = rminer::Importance( m, data )
+
+  } else{
+    #regression
+    imp = rminer::Importance(m, data, PRED = twidlr::predict.svm )
+  }
 
   df = tibble( row_names = names(data)
                , value   = imp$imp
@@ -105,7 +134,7 @@ f_model_importance_svm = function(m){
 #'
 #' @rdname f_model_importance_rpart
 #' @export
-f_model_importance_rpart = function(m){
+f_model_importance_rpart = function(m, ...){
 
   if( ! inherits(m, 'rpart') ){
     warning('model is not of class rpart returning NULL')
--- tests/testthat/test_model_feats.R
+++ tests/testthat/test_model_feats.R
@@ -3,17 +3,45 @@
 context('model features')
 
 
-test_that('model features regression'
+test_that('model importance regression'
           ,{
 
   m = twidlr::randomForest(mtcars, disp~.)
   f_model_importance_randomForest(m)
 
   m = twidlr::svm(mtcars, disp~.)
-  f_model_importance_svm(m)
+  f_model_importance_svm(m, mtcars)
 
   m = twidlr::rpart(mtcars, disp~.)
   f_model_importance_rpart(m)
 
-  hotzenplotz
+})
+
+
+test_that('model importance classification'
+          ,{
+
+  data_ls = f_clean_data(mtcars)
+
+  m = twidlr::randomForest(data_ls$data, cyl~.)
+  f_model_importance_randomForest(m)
+
+  m = twidlr::svm(data_ls$data, cyl~.)
+  f_model_importance_svm(m, data_ls$data )
+
+  m = twidlr::rpart(data_ls$data, cyl~.)
+  f_model_importance_rpart(m)
+
+  #classification with non numerics
+  data_ls = f_clean_data(mtcars)
+  data = data_ls$data
+  m = twidlr::svm(data, cyl~.)
+  f_model_importance_svm(m, data)
+
+  #classification with character variable
+  data = f_manip_matrix_2_tibble(mtcars)
+  data$cyl = factor(data$cyl, ordered = T)
+  m = twidlr::svm(data, cyl~.)
+  f_model_importance_svm(m, data)
+
 })
