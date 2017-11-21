e42b881d58df0605789d9d7296f113d0b4140e3c
Oettinghaus <Oettinghaus_B@holding.local>
2017-11-21 11:01:48 +0100

    fix in f_predict.R

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -37,20 +37,29 @@ test_that('plot model performance'
 
   form = as.formula( 'displacement~cylinders+mpg')
 
-  ISLR::Auto %>%
+  df = ISLR::Auto %>%
     pipelearner::pipelearner() %>%
     pipelearner::learn_models( twidlr::rpart, form ) %>%
     pipelearner::learn_models( twidlr::randomForest, form ) %>%
     pipelearner::learn_models( twidlr::svm, form ) %>%
     pipelearner::learn() %>%
     f_predict_pl_regression( 'name' ) %>%
-    unnest(preds) %>%
+    unnest(preds)
+
+  df %>%
     mutate( bins = cut(target1, breaks = 3 , dig.lab = 4)
             , title = paste(models.id, cv_pairs.id, train_p, target, model) ) %>%
     f_predict_plot_model_performance_regression() %>%
     f_plot_obj_2_html(type = 'taglist', 'test_me', title = 'Model Performance')
 
   file.remove('test_me.html')
+
+  # more than three bins
+
+  taglist = df %>%
+    mutate( bins = cut(target1, breaks = 6 , dig.lab = 4)
+            , title = paste(models.id, cv_pairs.id, train_p, target, model) ) %>%
+    f_predict_plot_model_performance_regression()
 
 })
 
